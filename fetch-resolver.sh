5c8a90b365f35343b417405d452a54a36d7c2313
o3oskye <posesion19@gmail.com>
2024-06-21 01:10:59 +0700

    first commit

---
+++ fetch-resolver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf resolvers.txt
+rm -rf resolvers-trusted.txt
+wget https://raw.githubusercontent.com/trickest/resolvers/main/resolvers.txt
+wget https://raw.githubusercontent.com/trickest/resolvers/main/resolvers-trusted.txt
